8b542bdc6c5a31d29d74c76ce7d23e4f605c0320
TshepoDavid Molefe <tshepomolefeda@gmail.com>
2021-11-02 22:28:31 +0200

    Use propTypes

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,43 +1,19 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
-const books = [
-  {
-    category: 'Commedy',
-    title: 'Hangover',
-    author: 'Suzanne Collins',
-    progress: {
-      currentChapter: 'Chapter 17',
-      completed: '64',
-    },
-  },
-  {
-    category: 'Olympics',
-    title: 'Tokyo',
-    author: 'Frank Herbert',
-    progress: {
-      currentChapter: 'Chapter 3: "A Lesson Learned"',
-      completed: '8',
-    },
-  },
-  {
-    category: 'Weather',
-    title: 'Capital in the Twenty-First Century',
-    author: 'Suzanne Collins',
-    progress: {
-      currentChapter: 'Introduction',
-      completed: '0',
-    },
-  },
-];
+const Page = ({ books }) => {
+  const {
+    category, title, author, progress,
+  } = books;
 
-const Page = () => (
-  <div className="books-container">
-    {books.map((element) => (
-      <div className="bookcard" key="element">
+  return (
+    <div className="books-container">
+      <div className="bookcard">
         <div className="book-left">
-          <h4 className="book_genre">{element.category}</h4>
-          <h3 className="title">{element.title}</h3>
-          <h6 className="author">{element.author}</h6>
+          <h4 className="book_genre">{category}</h4>
+          <h3 className="title">{title}</h3>
+          <h6 className="author">{author}</h6>
+          <h6 className="progress">{progress}</h6>
           <div className="actions">
             <button type="button" className="btn-special">
               Comments
@@ -53,20 +29,34 @@ const Page = () => (
         <div className="book-center">
           <img src="./" alt="progress" className="progress" />
           <div className="data">
-            <h3>{element.progress.completed}</h3>
-            <h6>Completed</h6>
+            <h3>{progress}</h3>
+            <h5>Completed</h5>
           </div>
         </div>
         <div className="book-right">
           <div>
             <h3>Current chapter</h3>
-            <h3>{element.progress.currentChapter}</h3>
+            <h3>{progress}</h3>
           </div>
           <button type="button">Update Progress</button>
         </div>
       </div>
-    ))}
-  </div>
-);
+
+    </div>
+  );
+};
+
+Page.propTypes = {
+  books: PropTypes.shape({
+    category: PropTypes.string,
+    title: PropTypes.string,
+    author: PropTypes.string,
+    progress: PropTypes.bool,
+  }),
+};
+
+Page.defaultProps = {
+  books: {},
+};
 
 export default Page;

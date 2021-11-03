327d1a79805d108c2554dc4849b3ca9820a5be66
TshepoDavid Molefe <tshepomolefeda@gmail.com>
2021-11-03 20:42:29 +0200

    Import redux-thunk

--- src/redux/cofigureStore.js
+++ src/redux/cofigureStore.js
@@ -1,6 +1,7 @@
 /* eslint import/no-extraneous-dependencies: ["error", {"peerDependencies": true}] */
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import booksReducer from './books/books';
 
 const reducer = combineReducers({
@@ -9,7 +10,7 @@ const reducer = combineReducers({
 
 const store = createStore(
   reducer,
-  applyMiddleware(logger),
+  applyMiddleware(thunk, logger),
 );
 
 export default store;

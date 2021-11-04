1ce5745aa6b2446e7cbc62220d78b8e797023fb5
TshepoDavid Molefe <tshepomolefeda@gmail.com>
2021-11-04 18:00:28 +0200

    Import adbook.css

--- src/components/Addbook.js
+++ src/components/Addbook.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 import { addBook } from '../redux/api/api';
+import './style/addbook.css';
 
 function Addbook() {
   const dispatch = useDispatch();

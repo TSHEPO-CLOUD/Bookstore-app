6e76dcc61efcba6293d203ae0e3635c2b4365a1b
TshepoDavid Molefe <tshepomolefeda@gmail.com>
2021-11-04 18:26:39 +0200

    Refactore header.css

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { removeBook, addBook } from '../redux/api/api';
+import './style/page.css';
 
 function Page() {
   const { booksReducer } = useSelector((state) => state);
